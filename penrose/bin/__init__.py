6181b51b1b74b898df112ac2fd352ed68f258b92
mvr <matt@example.com>
2019-07-20 22:54:23 -0400

    refactor

--- penrose/bin/__init__.py
+++ penrose/bin/__init__.py
@@ -16,80 +16,6 @@ import time
 
 LOGGER = penrose.util.get_logger(__name__)
 
-# minimal bash script to create a minimal houdini script
-# which will open up the engine to some remote control
-CMD_BOOTSTRAP = (
-    'echo "import hrpyc; hrpyc.start_server(use_thread=True, quiet=False)"'
-    '> .tmp.py ; houdini waitforui .tmp.py&')
-
-# minimal bootstrap code to make the remote engine
-# respect the same virtualenv as this runtime does.
-CODE_VENV = """## This code is run by houdini, which knows VIRTUAL_ENV via
-## env-vars, but does not by default honor it.  We need to setup
-## the path to use VENV libraries
-import os, sys
-venv = os.environ.get('VIRTUAL_ENV')
-if venv:
-    print "detected venv, adding to path.."
-    lib_dir = os.path.join(venv, 'lib')
-    for x in os.listdir(lib_dir):
-        path = os.path.join(lib_dir, x, 'site-packages')
-        print "  + {}".format(path)
-        sys.path.append(path)"""
-
-
-
-# @main.command(name='stl-render', help='show')
-# @click.option('--verbose', help='set logger to DEBUG', default=False, is_flag=True)
-# @click.option('--size', help='img size', default='1024', required=False)
-# # @click.option('-o', '--output-dir', help='output dir', required=False, default=None)
-# @click.argument('filename')
-# @click.pass_context
-# def stl_render(ctx, verbose, size, filename):
-#     LOGGER.debug("rendering filename: {}".format(filename))
-#     # cmd = '{} -b -P mesh2img.py -- --paths {} --dimensions {} --camera-coords 0.0,0.0,10.0'.format(BLENDER_EXEC, filename, size)
-#     # {filepath}_{width}.{ext}
-#     util.invoke(cmd)
-#     # from stl import mesh
-#     # from mpl_toolkits import mplot3d
-#     # from matplotlib import pyplot
-#     # # Create a new plot
-#     # figure = pyplot.figure()
-#     # axes = mplot3d.Axes3D(figure)
-#     # # Load the STL files and add the vectors to the plot
-#     # your_mesh = mesh.Mesh.from_file(filename)
-#     # axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-#     # # Auto scale to the mesh size
-#     # scale = your_mesh.points.flatten(-1)
-#     # axes.auto_scale_xyz(scale, scale, scale)
-#     # # Show the plot to the screen
-#     # pyplot.show()
-#
-#
-#
-# @main.command(name='stl-rotate', help='rotate')
-# @click.option('--verbose', help='set logger to DEBUG', default=False, is_flag=True)
-# @click.option('--step', help='step angle', required=False, default='10')
-# @click.option('-o', '--output-dir', help='output dir', required=False, default=None)
-# @click.argument('filename')
-# @click.pass_context
-# def stl_rotate(ctx, verbose, step, output_dir, filename):
-#     from stl import mesh
-#     stl_mesh = mesh.Mesh.from_file(filename)
-#     step = int(step)
-#     output_dir = output_dir or os.path.join(os.getcwd(),'output')
-#     if not os.path.isdir(output_dir):
-#         LOGGER.debug("saving to: ".format(output_dir))
-#         os.makedirs(output_dir)
-#
-#     rotation_axises = [0.0, 1.0, 0.0]
-#     iterations = int(360.0/step)
-#     for i in range(1, iterations):
-#         angle = step*i
-#         file = os.path.join(output_dir,'angle-{}.stl'.format(angle))
-#         LOGGER.debug(" angle {}: saving to {}".format(angle,file) )
-#         stl_mesh.rotate(rotation_axises, math.radians(angle))
-#         stl_mesh.save(file)
 
 from penrose.hx.engine import Engine
 ENGINE = Engine()
@@ -159,13 +85,9 @@ def houdini(engine=None, file=None, verbose=False, **kargs):
     errors = hx_test(file=file)
     if any(errors):
         raise RuntimeError(errors)
-    result = util.invoke(
-        cmd=CMD_BOOTSTRAP,
-        system=True,
-        environment=get_env())
-    ENGINE.exec_code(CODE_VENV)
+    ENGINE.init()
     ENGINE.exec_script(file=file)
-    remote_modules=ENGINE.import_remote_modules()
+    remote_modules = ENGINE.import_remote_modules()
     namespace = locals().copy()
 
     namespace.update(
--- penrose/hx/engine.py
+++ penrose/hx/engine.py
@@ -2,7 +2,8 @@
 penrose.hx.engine:
 """
 import sys
-from penrose import (util)
+from penrose import (util,)
+from penrose.hx import util as hx_util
 from .abcs  import HWrapper
 
 # placeholder placeholder
@@ -18,10 +19,37 @@ print 'exporting script execution namespace to var `hx.SCRIPT`'
 # these will be imported locally after rpyc is setup
 REMOTE_MODULES = 'hou hrpyc toolutils stateutils'.split()
 
+# minimal bash script to create a minimal houdini script
+# which will open up the engine to some remote control
+CMD_BOOTSTRAP = (
+    'echo "import hrpyc; hrpyc.start_server(use_thread=True, quiet=False)"'
+    '> .tmp.py ; houdini waitforui .tmp.py&')
+
+# minimal bootstrap code to make the remote engine
+# respect the same virtualenv as this runtime does.
+CODE_VENV = """## This code is run by houdini, which knows VIRTUAL_ENV via
+## env-vars, but does not by default honor it.  We need to setup
+## the path to use VENV libraries
+import os, sys
+venv = os.environ.get('VIRTUAL_ENV')
+if venv:
+    print "detected venv, adding to path.."
+    lib_dir = os.path.join(venv, 'lib')
+    for x in os.listdir(lib_dir):
+        path = os.path.join(lib_dir, x, 'site-packages')
+        print "  + {}".format(path)
+        sys.path.append(path)"""
 
 class Engine(HWrapper):
     """
     """
+    def init(self):
+        result = util.invoke(
+            cmd=CMD_BOOTSTRAP,
+            system=True,
+            environment=hx_util.get_env())
+        self.exec_code(CODE_VENV)
+
     @property
     def conn(self):
         """
@@ -44,7 +72,7 @@ class Engine(HWrapper):
             self.logger.debug("settings sys.modules[{}]={}".format(name, mod))
             sys.modules[name] = mod
         return remote_modules
-        
+
     def exec_script(self, file=None, **kwargs):
         """
         exec script in the houdini engine
